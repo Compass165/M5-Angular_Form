ca1998d4480ad6faca32b3c20e730e9665e1bbc0
Compass <ndphong165@gmail.com>
2022-11-21 15:19:08 +0700

    done interface

---
+++ Exercise/login-angular-form/src/app/interface/message.ts
@@ -0,0 +1,11 @@
+import {User} from "./user";
+
+export interface Message {
+
+  user: User;
+
+  date: Date;
+
+  message: string[];
+
+}
---
+++ Exercise/login-angular-form/src/app/interface/user.ts
@@ -0,0 +1,17 @@
+export interface User {
+
+  name: string;
+
+  email: string;
+
+  password: string;
+
+  age: number;
+
+  address: string;
+
+  image: string;
+
+  status: boolean;
+
+}
